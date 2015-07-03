4b0627a9f1fb19016e6383f65de4fc4bbe184061
Qing Wu <wiserfirst@gmail.com>
2015-07-03 22:37:13 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#My Dot files
+
+Supposed to be used together with [maximum-awesome](https://github.com/square/maximum-awesome). Just include customized configuration for Vim, Tmux and zsh.
---
+++ install.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+
+puts '============= Start ==========='
+Dir['.*'].each do |f|
+  if File.file? f
+    target = File.expand_path '~/' + f
+    if File.symlink? target
+      puts "removed symlink #{target}"
+      File.unlink target
+    else
+      backup = target + '.bak'
+      puts "backed up #{target} to #{backup}"
+      File.rename target, backup
+    end
+    File.symlink File.expand_path(f), target
+  end
+end
+
+# config git
+puts '============= Config Git ==========='
+configs = {
+  'alias.st'     => 'status',
+  'alias.br'     => 'branch',
+  'alias.rb'     => 'rebase',
+  'alias.co'     => 'checkout',
+  'alias.ci'     => 'commit',
+  'alias.brv'    => 'branch -v',
+  'core.editor'  => 'vim',
+  'push.default' => 'simple',
+}
+configs.each do |key, value|
+  system "git config --global #{key} #{value}"
+end
+
+puts '============= DONE ============'
